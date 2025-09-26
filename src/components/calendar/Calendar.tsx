f49fb94272014d34fc25ff818fe2e34e6cd63dbf
daily-coco <dailydev.sw@gmail.com>
2025-09-26 11:17:32 +0900

    feat : Calendar 마크업 작업 중 커밋

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,14 +1,33 @@
 import styled from 'styled-components';
 
 interface CalendarDataProps {
-  marredMonth: number;
+  year: number;
+  month: number; // 1~12
 }
 
 const Calendar = ({ ...props }: CalendarDataProps) => {
+  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+
+  // 이번 달 정보
+
   return (
     <CalendarWrapper>
       <CalendarBody>
-        <CalendarMonth>{props.marredMonth}</CalendarMonth>
+        <CalendarTable>
+          <TableCaption>{`${props.month}월 달력`}</TableCaption>
+          <TableHeader>
+            <TableRow>
+              {daysOfWeek.map((week) => (
+                <TableHeaderCell key={week} scope='col'>
+                  {week}
+                </TableHeaderCell>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableTbody>
+            <TableTbodyTd></TableTbodyTd>
+          </TableTbody>
+        </CalendarTable>
       </CalendarBody>
     </CalendarWrapper>
   );
@@ -18,4 +37,22 @@ export default Calendar;
 
 const CalendarWrapper = styled.section``;
 const CalendarBody = styled.div``;
-const CalendarMonth = styled.strong``;
+const CalendarTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  table-layout: fixed;
+`;
+
+const TableHeader = styled.thead`
+  background: #f9f9f9;
+`;
+const TableCaption = styled.caption``;
+const TableRow = styled.tr``;
+const TableHeaderCell = styled.th`
+  padding: 8px;
+  font-weight: bold;
+  text-align: center;
+`;
+const TableTbody = styled.tbody``;
+
+const TableTbodyTd = styled.td``;
